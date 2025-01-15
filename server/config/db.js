2ceec481987b4d9a7de7a60af7658227a69678fc
zandanbatzorig <49382839+zandanbatzorig@users.noreply.github.com>
2025-01-16 02:25:15 +0800

    changed

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,19 @@ const connectDB = async () => {
   
 try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect('mongodb+srv://raddy:N7cJGzWpEtLxFfXT@cluster0.be7ts.mongodb.net/blog');
+    const conn = await mongoose.connect('mongodb+srv://raddy:N7cJGzWpEtLxFfXT@cluster0.be7ts.mongodb.net/blog',{
+        serverSelectionTimeoutMS: 20000,
+        socketTimeoutMS: 45000,
+    });
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
+    console.error('Failed to connect to MongoDB:', error);
   }
+
+
+
+  
 
 }
 
